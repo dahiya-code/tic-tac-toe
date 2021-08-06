f6d67993deb7cf83e85b7e6499baf5b604a90319
Navya Dahiya <d.navya2312@gmail.com>
2021-08-06 18:17:06 +0530

    alert+OptnXO+PlayArea

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+let human = 1,
+  comp = 0;
+let level = -1;
+let moves = 0;
+// bot[], player1[] and player2[] stores the magic number of corresponding move of bot, player1 and player2 respectively
+let bot = [];
+let player1 = [];
+let player2 = [];
+let occupied = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let flag = 0;
+let flag1 = 0;
+const magicsquare = [2, 7, 6, 9, 5, 1, 4, 3, 8];
+
+const vsPlayer = (n) => {
+  let x = document.getElementById("but0");
+  let y = document.getElementById("but1");
+  if (moves & 1) {
+  }
+};
+
+const clicked = (n) => {
+  console.log(n);
+  if (level === -1) {
+    alert("Select 'Vs Bot' or 'Vs Player' first");
+    return;
+  }
+  if (occupied[n - 1]) return;
+  moves++;
+  let x = document.getElementById(n);
+  let sign;
+  if (level === 0) {
+    sign = moves % 2;
+    x.textContent = "X";
+    vsPlayer(n);
+  } else {
+    //BOT
+  }
+};
+
 const hover = () => {
   let y = document.getElementById("levels");
   y.style.visibility = "visible";
@@ -12,4 +51,11 @@ const hover = () => {
 const notHover = () => {
   let y = document.getElementById("levels");
   y.style.visibility = "hidden";
+  //   if (level <= 0) {
+  //     var x = document.getElementById("button1");
+  //     x.style.backgroundColor = "#A9A9A9";
+  //   } else {
+  //     var x = document.getElementById("button1");
+  //     x.style.backgroundColor = "deepPink";
+  //   }
 };
