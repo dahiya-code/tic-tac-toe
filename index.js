aec27633ffd06f62fc552966bd7b65d0ec42945b
Navya Dahiya <d.navya2312@gmail.com>
2021-08-06 12:45:49 +0530

    versus static

---
+++ index.js
@@ -0,0 +1,15 @@
+const hover = () => {
+  let y = document.getElementById("levels");
+  y.style.visibility = "visible";
+  //   if (level <= 0) {
+  //     let x = document.getElementById("b1");
+  //     x.style.backgroundColor = "#808080";
+  //   } else {
+  //     let x = document.getElementById("b1");
+  //     x.style.backgroundColor = "#C71585";
+  //   }
+};
+const notHover = () => {
+  let y = document.getElementById("levels");
+  y.style.visibility = "hidden";
+};
